ef33e003c047f4db7281b92ffd064c71e3e358f0
EXANT2025 <kimkijug@gmail.com>
2026-04-24 15:59:02 +0900

    Create safe-analytics.tsx

---
+++ components/safe-analytics.tsx
@@ -0,0 +1,5 @@
+// Analytics disabled due to "mt is not iterable" error in @vercel/analytics
+// This can be re-enabled when the package issue is resolved
+export function SafeAnalytics() {
+  return null
+}
